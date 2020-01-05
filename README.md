7ecebcb9a92e6f3fb449d519c2e6055ab40e3827
inoval calabia <inovalcalabia@gmail.com>
2020-01-05 14:46:12 +0800

    refactor

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # leader-board
 
 ## How to run
+
 - just simply open the index.html file
 
 ## Live
+
 - http://www.inovalcalabia.me/leader-board/
+
+## Author
+
+👤 **inovalcalabia**
--- scripts/index.js
+++ scripts/index.js
@@ -1,69 +1,55 @@
-
 var boxCount = 10;
 var currentIndex = 5;
 var boxList = [];
 var activeClass = 'active-box';
 var inactiveClass = 'in-active-box';
+
 function initBoxes() {
     for (var i = 1; i <= boxCount; i += 1) {
-        document.getElementsByClassName('container')[0].innerHTML += ('<div class="box-list box-index-' + i+'" value='+i+'>' + i + '</div>');
-        boxList.push(i);
-    }
-}
-function initHandler() {
-    for (var i = 1; i <= boxCount; i += 1) {
-        document.getElementsByClassName('box-index-' + i)[0].addEventListener('click', onClickHandler); 
+        var box = document.createElement("div");
+        box.className = 'box';
+        box.textContent = i;
+        box.onclick = onClickHandler;
+        box.setAttribute('value', i);
+        document.getElementsByClassName('container')[0].appendChild(box);
+        boxList.push(box);
     }
 }
 function onClickHandler(e) {
-    currentIndex = e.target.getAttribute('value');
-    update();
-}
-function indexHandler() {
-    if (currentIndex < 1) {
-        currentIndex = 1;
-    } else if (currentIndex > boxCount) {
-        currentIndex = boxCount;
-    }
-    update();
+    currentIndex = Number(e.target.getAttribute('value'));
+    render();
 }
 function inactive(value) {
-    return value > currentIndex;
+    return Number(value.getAttribute('value')) > currentIndex;
 }
 function equals(value) {
-    return value == currentIndex;
+    return Number(value.getAttribute('value')) == currentIndex;
 }
 function clean() {
-    document.querySelectorAll(".box-list").forEach(function(element) {
+    document.querySelectorAll(".box").forEach(function(element) {
         element.classList.remove(activeClass);
         element.classList.remove(inactiveClass);
     });
 }
-function update() {
+function render() {
     clean();
-    boxList.filter(inactive).forEach((v)=>{document.getElementsByClassName('box-index-' + v)[0].classList.add(inactiveClass);})
-    boxList.filter(equals).forEach((v)=>{document.getElementsByClassName('box-index-' + v)[0].classList.add(activeClass);})
-  
+    boxList.filter(inactive).forEach((v)=>{v.classList.add(inactiveClass);})
+    boxList.filter(equals).forEach((v)=>{v.classList.add(activeClass);})
 }
 function keyDownHandler(e) {
-    switch(e.keyCode) {
-        case 38:
-          currentIndex -= 1;
-          indexHandler();
-          break;
-        case 40:
-          currentIndex += 1;
-          indexHandler();
-          break;
-        default:
-      }
+    if (e.keyCode === 38 && currentIndex > 1) {
+        currentIndex -= 1;
+    }
+    if (e.keyCode === 40 && currentIndex < boxCount) {
+        currentIndex += 1;
+    }
+    render();
 }
 function init() {
     initBoxes();
-    initHandler();
-    update();
+    render();
 }
 if (document.addEventListener) {
-    document.addEventListener('DOMContentLoaded', init, false);
+    document.addEventListener('DOMContentLoaded', init);
     document.addEventListener('keydown', keyDownHandler);
 }
